9a874ea495a7bc2a7f65ed4bd9ceb572cb86c8b0
Harshjeet Kumar <Harshjeet.Kumar@valtech.com>
2022-10-11 18:02:59 +0530

    updated

---
+++ MovieAppCoreApi/Controllers/MoviesController.cs
@@ -0,0 +1,44 @@
+﻿using BookMyShowBussiness.services;
+using BookMyShowEntity;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace MovieAppCoreApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class MoviesController : ControllerBase
+    {
+        private MovieService _movieService;
+        public MoviesController(MovieService movieService)
+        {
+            _movieService = movieService;
+        }
+        [HttpGet("GetMovies")]
+        public IEnumerable<Movie> GetMovies()
+        {
+            return _movieService.GetMovies();
+        }
+
+        [HttpPost("AddMovie")]
+        public IActionResult AddMovie([FromBody] Movie movie)
+        {
+            _movieService.AddMovie(movie);
+            return Ok("Movie created successfully!!");
+        }
+        [HttpDelete("DeleteMovie")]
+        public IActionResult DeleteMovie(int movieId)
+        {
+            _movieService.DeleteMovie(movieId);
+            return Ok("Movie deleted successfully!!");
+        }
+        [HttpPut("UpdateMovie")]
+        public IActionResult UpdateMovie([FromBody] Movie Movie)
+        {
+            _movieService.UpdateMovie(Movie);
+            return Ok("Movie updated successfully!!");
+        }
+    }
+}
---
+++ MovieAppCoreApi/Controllers/ShowTimingsController.cs
@@ -0,0 +1,43 @@
+﻿using BookMyShowBussiness.services;
+using BookMyShowEntity;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+
+namespace MovieAppCoreApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ShowTimingsController : ControllerBase
+    {
+        private ShowTimingService _showTimingService;
+        public ShowTimingsController(ShowTimingService showTimingService)
+        {
+            _showTimingService = showTimingService;
+        }
+        [HttpGet("GetShowTimings")]
+        public IEnumerable<ShowTiming> GetShowTimings()
+        {
+            return _showTimingService.GetShowTimings();
+        }
+
+        [HttpPost("AddShowTiming")]
+        public IActionResult AddShowTiming([FromBody] ShowTiming showTiming)
+        {
+            _showTimingService.AddShowTiming(showTiming);
+            return Ok("ShowTiming created successfully!!");
+        }
+        [HttpDelete("DeleteShowTiming")]
+        public IActionResult DeleteShowTiming(int showtimingId)
+        {
+            _showTimingService.DeleteShowTiming(showtimingId);
+            return Ok("ShowTiming deleted successfully!!");
+        }
+        [HttpPut("UpdateShowTiming")]
+        public IActionResult UpdateShowTiming([FromBody] ShowTiming showTiming)
+        {
+            _showTimingService.UpdateShowTiming(showTiming);
+            return Ok("ShowTiming updated successfully!!");
+        }
+    }
+}
